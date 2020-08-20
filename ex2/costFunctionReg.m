4bd11dc30ed273c3910c96d7cf8280d725b72dec
stevenguyen72 <69959501+stevenguyen72@users.noreply.github.com>
2020-08-20 16:56:33 +0700

    Add files via upload

---
+++ ex2/costFunctionReg.m
@@ -0,0 +1,39 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+a = X * theta;
+hox = sigmoid(a);
+c = -(y.*log(hox) + (1-y).* log(1-hox));
+d = (1/m) * sum(c);
+e = (lambda/(2*m)) * sum(theta(2:end).^2);
+J = d + e;
+[n2,m2] = size(X);
+i = 2;
+grad(1) = (1/m) * (sum((hox - y) .* X(:,1)));
+while i <= m2 
+    grad(i) = (1/m) * (sum((hox - y) .* X(:,i))) + (lambda/m)*theta(i);
+    i = i + 1;
+
+
+
+
+
+
+% =============================================================
+
+end
---
+++ ex2/plotData.m
@@ -0,0 +1,33 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+a = find(y==1);
+b = find(y==0);
+pos = X(a,:);
+neg = X(b,:);
+plot(pos(:,1),pos(:,2),'k+','MarkerSize',7,'LineWidth',2)
+plot(neg(:,1),neg(:,2),'ko','MarkerSize',7,'MarkerFaceColor','y')
+
+
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
